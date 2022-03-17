57575db432657b39bfb1176aa956c0d4c2dac0c8
miatto88 <miatto0788@gmail.com>
2022-03-17 23:44:24 +0900

    factoryでダミーデータの作成

---
+++ my-laravel-app/database/factories/TodoFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Todo;
+use Faker\Generator as Faker;
+
+$factory->define(Todo::class, function (Faker $faker) {
+    return [
+        "user_id" => $faker->randomElement([1, 2, 3]),
+        "title" => $faker->randomElement([
+            "Laravel勉強",
+            "git勉強",
+            "引っ越し準備",
+            "服買い出し"
+        ])
+    ];
+});
--- my-laravel-app/database/factories/UserFactory.php
+++ my-laravel-app/database/factories/UserFactory.php
@@ -19,10 +19,11 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'name' => $faker->randomElement(["tanaka", "yamada", "nakayama"]),
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'role' => $faker->randomElement([0, 1]),
         'remember_token' => Str::random(10),
     ];
 });
