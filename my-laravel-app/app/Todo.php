268a5440b0fc384cd41825eaa52a1fba82b4f478
miatto88 <miatto0788@gmail.com>
2022-05-15 17:26:57 +0900

    一覧画面の作成

--- my-laravel-app/app/Todo.php
+++ my-laravel-app/app/Todo.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     protected $table = "todos";
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
 }
---
+++ my-laravel-app/resources/views/todo/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+
+<body>
+    <table border='1'>
+        <tr>
+            <th width='200'>タスク名</th>
+            <th width='200'>担当者</th>
+            <th width='250'>登録日</th>
+            <th width='100'>詳細</th>
+        </tr>
+        @foreach ($records as $record)
+        <tr>
+            <th>{{ $record->title }}</th>
+            <th>{{ $record->user->name }}</th>
+            <th>{{ $record->created_at }}</th>
+            <th><a href="">リンク</a></th>
+        </tr>
+        @endforeach
+    </table>
+</body>
+</html>
